caca31b9f2523c28ddcf3af39dc69bde94b3071f
Pr.Walter Bulbazor <81115582+PrWalterB@users.noreply.github.com>
2022-11-24 02:37:48 +0100

    Update larbs.sh

--- larbs.sh
+++ larbs.sh
@@ -24,8 +24,8 @@ error() {
 }
 
 welcomemsg() {
-	whiptail --title "Welcome!" \
-		--msgbox "Welcome to Luke's Auto-Rice Bootstrapping Script!\\n\\nThis script will automatically install a fully-featured Linux desktop, which I use as my main machine.\\n\\n-Luke" 10 60
+	whiptail --title "La Benvenguda!" \
+		--msgbox "La Benvenguda dins le Luke's Auto-Rice Bootstrapping Script!\\n\\nThis script will automatically install a fully-featured Linux desktop, which I use as my main machine.\\n\\n-Luke" 10 60
 
 	whiptail --title "Important Note!" --yes-button "All ready!" \
 		--no-button "Return..." \
